7c885cd581e97a2c74daaa4479b01f91ce178aae
Mikhail Metrikin <metrikin@gmail.com>
2017-05-29 11:46:49 -0700

    Wip dev build

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+/* eslint-disable no-console */
+console.log('Helo Linter');
+console.log(4);
+var x = 4;
+
+var y = function y(x) {
+  return x + 4;
+};
+
+console.log("Working?", y(x));
