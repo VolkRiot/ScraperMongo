835a7b4ae9c6942db9cb8781287dc4e9dd02bcf4
Mikhail Metrikin <metrikin@gmail.com>
2017-05-29 11:47:55 -0700

    Saving point before tool expansion

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,4 @@
-"use strict";
+'use strict';
 
 /* eslint-disable no-console */
-console.log('Helo Linter');
-console.log(4);
-var x = 4;
-
-var y = function y(x) {
-  return x + 4;
-};
-
-console.log("Working?", y(x));
+console.log('Hello Linter');
