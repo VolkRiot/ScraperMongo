115a451fb40f016ded7bbcc6aff8a25348e41a87
Mikhail Metrikin <metrikin@gmail.com>
2017-05-29 21:38:29 -0700

    Adding minor config changes

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,39 @@
 'use strict';
 
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var PORT = process.env.PORT || 8080;
+//import exphbs from 'express-handlebars';
 /* eslint-disable no-console */
-console.log('Hello Linter');
+// Compression?
+
+
+var app = (0, _express2.default)();
+
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: true }));
+app.use(_bodyParser2.default.text());
+app.use(_bodyParser2.default.json({ type: 'application/vnd.api+json' }));
+
+// app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use(_express2.default.static(_path2.default.join(__dirname, 'public')));
+
+// app.get('/test', (req, res) => {
+//   console.log('Route hit');
+// });
+
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
+});
--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 /* eslint-disable no-console */
-console.log('Hello Linter');
+// Compression?
+import express from 'express';
+import bodyParser from 'body-parser';
+// import exphbs from 'express-handlebars';
+import path from 'path';
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/test', (req, res) => {
+//   console.log('Route hit');
+// });
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
